16159c50c46785d188118fec35c4d0fba9695492
xwwqq <37721199+xwwqq@users.noreply.github.com>
2019-04-14 19:58:11 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 from app import app
+from app import app
 from exts import db
 from models import User,Question,Answer
 
